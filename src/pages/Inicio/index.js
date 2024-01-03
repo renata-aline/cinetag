068a4b9714bd39532b2316d561ad7a947ad30480
renata-aline <renatalinec@gmail.com>
2024-01-03 10:52:46 -0300

    construido rodape da pagina inicial

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -1,8 +1,12 @@
 import Cabecalho from "components/Cabecalho";
+import Rodape from "components/Rodape";
 
-function Inicio(){
-    return(
-       <Cabecalho /> 
-    )
+function Inicio() {
+  return (
+    <>
+      <Cabecalho />
+      <Rodape />
+    </>
+  );
 }
 export default Inicio;
