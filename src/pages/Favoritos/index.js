c9750134a5eba254a2012477f4b03b7b432ea5f3
renata-aline <renatalinec@gmail.com>
2024-01-08 09:55:14 -0300

    adicionado  texto e imagem a pagina de favoritos

--- src/pages/Favoritos/index.js
+++ src/pages/Favoritos/index.js
@@ -1,10 +1,24 @@
-import styles from './Favoritos.module.css';
+import Banner from "components/Banner";
+import Titulo from "components/Titulo";
+import Card from "components/Card";
+import styles from "./Favoritos.module.css";
 
 function Favoritos() {
-    return(
-        <>
-        </>
-    )
+  return (
+    <>
+      <Banner imagens="favoritos" />
+      <Titulo>
+        <h1>Meus Favoritos</h1>
+      </Titulo>
+      <section className={styles.container}>
+        <Card
+          id="1"
+          titulo="gato"
+          capa="https://thecatapi.com/api/images/get?format=src&type=png"
+        />
+      </section>
+    </>
+  );
 }
 
-export default Favoritos
+export default Favoritos;
