67b7e9adde7fd5efbe90ff6ebc066dde167e3f8f
renata-aline <renatalinec@gmail.com>
2024-01-31 11:29:59 -0300

    rota dinâmica

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -2,6 +2,7 @@ import { useFavoritoContext } from "components/contextos/Favoritos";
 import styles from "./Card.module.css";
 import iconeFavoritar from './favoritar.png';
 import iconeDesfavoritar from './desfavoritar.png';
+import { Link } from "react-router-dom";
 
 function Card({ id, titulo, capa }) {
   const {favorito, adicionarFavorito} = useFavoritoContext();
@@ -9,8 +10,10 @@ function Card({ id, titulo, capa }) {
   const icone = !ehfavorito ? iconeFavoritar : iconeDesfavoritar;
   return (
     <div className={styles.container}>
+      <Link className={styles.link} to={`/${id}`}>
       <img src={capa} alt={titulo} className={styles.capa} />
       <h2>{titulo}</h2>
+      </Link>
       <img
         src={icone}
         alt="Favoritar filme"
--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import Rodape from "components/Rodape";
 import FavoritosProvider from "components/contextos/Favoritos";
 import Favoritos from "pages/Favoritos";
 import Inicio from "pages/Inicio";
+import Player from "pages/Player";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function AppRoutes() {
@@ -15,6 +16,7 @@ function AppRoutes() {
         <Routes>
           <Route path="/" element={<Inicio />}></Route>
           <Route path="/favoritos" element={<Favoritos />}></Route>
+          <Route path="/:id" element={<Player/>}></Route>
         </Routes>
         </FavoritosProvider>
       </Container>
